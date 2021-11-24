fda20e2fb7b23e1fa9618f046cb1f1a4bb39f5d0
Guilherme Wilson Bezerra <52801217+GWilsonB@users.noreply.github.com>
2021-11-24 18:36:50 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+	<script link="stylesheet" href="css\bootstrap.min.css"></script>
+	<script link="stylesheet" href="js\bootstrap.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
