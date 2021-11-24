4df6b6076c4ceddc666f5777d2fd7edbcfb36a85
PPonte5 <62364164+PPonte5@users.noreply.github.com>
2021-11-24 20:17:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,25 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 	<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-	<script link="stylesheet" href="css\bootstrap.min.css"></script>
-	<script link="stylesheet" href="js\bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css\bootstrap.min.css">
+	<link rel="stylesheet" href="js\bootstrap.min.js">
 </head>
 <body>
+	<div class="row" class="justify-content-center">
+		<form action="process.php" method="POST"></form>
+		<div class="form-group">
+			<label>Nome</label>
+			<input type="text" name="name" class="form-control" value="Insira seu nome">
+		</div>
 
+		<div class="form-group"></div>
+			<label>Localização</label>
+			<input type="text" name="location" class="form-control" value="Insira sua localização">
+		</div>	
+
+		<div class="form-group"></div>		
+			<button type="submit" class="btn btn-primary" name="save">Salvar</button>
+		</div>
+	</div>
 </body>
 </html>
